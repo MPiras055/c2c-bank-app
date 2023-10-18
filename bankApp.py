f5a2f195653125b4d9d46fdcfa9fa01c69c332c2
Mattia Piras <109771384+MPiras055@users.noreply.github.com>
2023-10-19 01:29:18 +0200

    Update bankApp.py
    Followed the points you put.
    
    Have a nice day

--- bankApp.py
+++ bankApp.py
@@ -8,19 +8,26 @@ login_types = ["admin", "user", "guest"]
 # For “guest”:
 # program says “You have no privileges.”
 def gatekeeper(login):
-  if login == "admin":
-    return "You have the privileges"
+  if login == "admin": return "You have the privileges."
+  elif login =="user": return "You have limited privileges."
+  else: return "You have no privileges"
+  
 
 # 3. Call the gatekeeper function with a string and print what it returns.
 print(gatekeeper("admin"))
+print(gatekeeper("user"))
+print(gatekeeper("guest"))
 
 # 4. How could this code be improved? Make it better. Think about what other scenarios you should cover in your if logic.
 
 # 5. Complete the function called check_balance that takes one parameter, loan_balance.
-def check_balance(loan_balance):
 # 6. If loan_balance is zero or more, it says “you don’t owe any money”
 # 7. If loan_balance is negative, it  says “you owe $X” where X is the amount
-    return
+
+def check_balance(loan_balance):
+  if(loan_balance>=0)return "you don't owe any money"
+  else return f"you owe ${-loan_balance}"
 
 # 8. Call your function with a negative and positive number and print what it returns.
 print(check_balance(300))
+print(check_balance(-300))
